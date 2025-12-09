b584a5a7422a9a2b243e2878e7119947f8822765
Henrique <hassis.moreira177@hotmail.com>
2025-12-08 23:36:07 -0300

    fix: corrigindo o bug que causava erro sintatico

--- Executavel.bat
+++ Executavel.bat
@@ -1,11 +1,11 @@
 @echo off
 
 echo 1. Gerando o Parser (Bison)...
-win_bison -d parser.y
+bison -d parser.y
 
 echo.
 echo 2. Gerando o Lexer (Flex)...
-win_flex lexer.l
+flex lexer.l
 
 echo.
 echo 3. Compilando e Linkando (GCC)...
